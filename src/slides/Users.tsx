36730cf443e2fcde7530149ff843fe73c999b19f
Almoayyad <Almoayyad9@gmail.com>
2026-09-14 20:00:57 +0300

    Slide 4 adds the Transport General Authority logo
    A fifth stakeholder tile, placed with the other government bodies before
    Absher and Nafath. Its SVG is cropped from a 720x720 canvas to its drawing.

--- src/slides/Users.tsx
+++ src/slides/Users.tsx
@@ -2,11 +2,13 @@ import absher from '../assets/stakeholders/absher.svg';
 import hrsd from '../assets/stakeholders/hrsd.svg';
 import moc from '../assets/stakeholders/moc.svg';
 import nafath from '../assets/stakeholders/nafath.svg';
+import tga from '../assets/stakeholders/tga.svg';
 import { PRIMARY_USERS } from '../content';
 
 const STAKEHOLDER_LOGOS = [
   { src: moc, name: 'Ministry of Commerce' },
   { src: hrsd, name: 'Ministry of Human Resources and Social Development' },
+  { src: tga, name: 'Transport General Authority' },
   { src: absher, name: 'Absher' },
   { src: nafath, name: 'Nafath' },
 ];
